a6fa07ad94930f0bac6f9b3b178a36be90de3742
Jossic <jossic.lapierre@gmail.com>
2022-02-03 22:06:26 +0100

    Essai test

--- App.spec.tsx
+++ App.spec.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
+// import React from 'react';
+// import { render } from '@testing-library/react-native';
 
-import App from './App';
+// import App from './App';
 
-jest.useFakeTimers();
+// jest.useFakeTimers();
 
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
 
+// describe('<App />', () => {
+//   it('has 1 child', () => {
+//     const tree: any = render(<App />).toJSON();
+//     // console.log(`tree =>`, tree);
+//     // @ts-ignore
+//     expect(tree.children.length).toBe(1);
+//   });
+// });
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import App from './App';
+
 describe('<App />', () => {
   it('has 1 child', () => {
-    const tree: any = render(<App />).toJSON();
-    // console.log(`tree =>`, tree);
-    // @ts-ignore
+    const tree: any = renderer.create(<App />).toJSON();
     expect(tree.children.length).toBe(1);
   });
 });
--- jest.config.ts
+++ jest.config.ts
@@ -1,28 +1,12 @@
 import { Config } from '@jest/types';
 
-// By default, all files inside `node_modules` are not transformed. But some 3rd party
-// modules are published as untranspiled, Jest will not understand the code in these modules.
-// To overcome this, exclude these modules in the ignore pattern.
-
 const config: Config.InitialOptions = {
-  testEnvironment: 'jsdom',
-  preset: 'jest-expo',
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-async-storage)',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!**/coverage/**',
-    '!**/node_modules/**',
-    '!**/babel.config.js',
-    '!**/jest.setup.js',
-  ],
-  coverageReporters: ['json-summary', 'text', 'lcov'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  setupFiles: ['./setupJest.js'],
 };
 
 export default config;

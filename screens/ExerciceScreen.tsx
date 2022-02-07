6260d13d9e388add1880422208f8fb59183ecffb
Jossic <jossic.lapierre@gmail.com>
2022-02-07 21:19:45 +0100

    changing floating button package

--- screens/ExerciceScreen.tsx
+++ screens/ExerciceScreen.tsx
@@ -1,6 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect } from 'react';
-import { Ionicons } from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import {
   FlatList,
   Image,
@@ -13,14 +14,32 @@ import { useDispatch, useSelector } from 'react-redux';
 import Exercice from '../components/Exercice/Exercice';
 import Colors from '../constants/Colors';
 import * as workoutActions from '../store/actions/workoutActions';
+import { FloatingAction } from 'react-native-floating-action';
+import { RootTabScreenProps } from '../types';
 
 interface ExerciceScreenProps {}
 
-const ExerciceScreen: React.FC<ExerciceScreenProps> = ({ navigation }) => {
-  const exercices = useSelector((state) => state.workout.exercices);
-  const userId = useSelector((state) => state.auth.userId);
-  const token = useSelector((state) => state.auth.token);
+const ExerciceScreen = ({ navigation }: RootTabScreenProps<'Exercice'>) => {
+  const exercices = useSelector((state) => state.workout?.exercices);
+
+  const userId = useSelector((state) => state.auth?.userId);
+  const token = useSelector((state) => state.auth?.token);
   const dispatch = useDispatch();
+
+  const actions = [
+    {
+      text: 'Ajouter un exercice',
+      icon: <Icon name="barbell" style={styles.actionButtonIcon} />,
+      name: 'bt_exercices',
+      position: 1,
+    },
+    {
+      text: 'Ajouter une séance',
+      icon: <Icon name="contract" style={styles.actionButtonIcon} />,
+      name: 'bt_workout',
+      position: 2,
+    },
+  ];
 
   useEffect(() => {
     dispatch(workoutActions.getExercices(userId, token));
@@ -34,21 +53,32 @@ const ExerciceScreen: React.FC<ExerciceScreenProps> = ({ navigation }) => {
         renderItem={({ item }) => <Exercice item={item} />}
       />
 
-      {/* <TouchableOpacity
-        activeOpacity={0.8}
-        onPress={() => navigation.navigate('Add Exercice')}
-      >
-        <LinearGradient colors={Colors.linear} style={styles.addButton}>
-          <Text style={styles.addButtonText}>Ajouter un exercice</Text>
-        </LinearGradient>
-      </TouchableOpacity> */}
-      <TouchableOpacity
-        activeOpacity={0.8}
-        style={styles.logout}
-        // onPress={onLogoutPressHandler}
-      >
-        <Ionicons name="add-outline" color="white" size={23} />
-      </TouchableOpacity>
+      <FloatingAction
+        actions={actions}
+        onPressItem={(name) => {
+          console.log(`selected button: ${name}`);
+        }}
+      />
+
+      {/*  <View style={{ flex: 1 }}>
+        
+        <ActionButton buttonColor={Colors.primary} testID="plusButton">
+          <ActionButton.Item
+            buttonColor="#9b59b6"
+            title="Ajouter un exercice"
+            onPress={() => console.log('press')}
+          >
+            <Icon name="barbell" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            buttonColor="#3498db"
+            title="Créer une séance"
+            onPress={() => navigation.navigate('Add Training')}
+          >
+            <Icon name="contract" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+               </ActionButton>
+      </View> */}
     </View>
   );
 };
@@ -71,15 +101,10 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
   },
-  logout: {
-    backgroundColor: Colors.primary,
-    height: 50,
-    width: 50,
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-    alignSelf: 'center',
-    position: 'absolute',
-    bottom: 50,
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+    // backgroundColor: Colors.primaryFaded,
   },
 });
--- screens/__tests__/ExerciceScreen.spec.tsx
+++ screens/__tests__/ExerciceScreen.spec.tsx
@@ -1,4 +1,26 @@
+import ExerciceScreen from '../ExerciceScreen';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
 describe('Séances & exercices screen', () => {
+  const initialState = {
+    exercices: [],
+  };
+  const mockStore = configureStore();
+  let store;
+
   //Doit y avoir un bouton + en bas
+  it('should have a + button', () => {
+    store = mockStore(initialState);
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <ExerciceScreen />
+      </Provider>
+    );
+    // const button = getByTestId('plusButton');
+    // expect(button).toBeTruthy();
+    expect(getByTestId('plusButton')).toBeDefined();
+  });
   //Doit pouvoir ajouter un exo, ou une seance
 });
